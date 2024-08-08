92d653fc3dad167c50f87e1eb015f34338c16456
Teddy <teddy.nutech@gmail.com>
2024-08-08 12:00:40 +0700

    up

--- api/index.go
+++ api/index.go
@@ -3,8 +3,17 @@ package handler
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/teddys48/kmpro/helper"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	server := http.NewServeMux()
 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+
+	server.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		helper.ReturnResponse(w, "Welcomeee!")
+	})
+
+	server.ServeHTTP(w, r)
 }
